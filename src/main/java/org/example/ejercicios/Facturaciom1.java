7e1a950f1076c7d82084e0e435faf8152cd4cc77
RichardoRodLoa <148672693+RichardoRodLoa@users.noreply.github.com>
2023-10-23 01:44:38 -0500

    Update Facturaciom1.java
    RICARDO RODRIGUEZ LOAIZA

--- src/main/java/org/example/ejercicios/Facturaciom1.java
+++ src/main/java/org/example/ejercicios/Facturaciom1.java
@@ -3,29 +3,39 @@ package org.example.ejercicios;
 import java.util.List;
 
 public class Facturaciom1 {
-	record Factura(String cliente, String fechafactura, List<Double> precioProducto) {
-	}
+    // Definición del registro "factura" para almacenar información de las facturas.
+    record factura(String cliente, String fechaFactura, List<Double> preciosProductos) {
+    }
 
-	public static void main(String[] args) {
-		Factura factura1 = new Factura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
-		Factura factura2 = new Factura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D));
+    public static void main(String[] args) {
+        // Creación de dos instancias de la clase "factura".
+        factura factura1 = new factura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
+        factura factura2 = new factura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D));
 
-		double totalValorFact1 = 0;
-		for (Double precio : factura1.precioProducto) {
-			totalValorFact1 += precio;
-		}
-		System.out.println("Factura 1" );
-		System.out.println("Cliente: " + factura1.cliente());
-		System.out.println("Total: " + totalValorFact1);
+        // Cálculo del total de la factura 1 y factura 2.
+        double totalValorFactura1 = calcularTotal(factura1);
+        double totalValorFactura2 = calcularTotal(factura2);
 
+        // Impresión de la factura 1 y factura 2.
+        imprimirFactura(factura1, totalValorFactura1);
+        imprimirFactura(factura2, totalValorFactura2);
+    }
 
-		double totalValorFact2 = 0;
-		for (Double precio : factura2.precioProducto) {
-			totalValorFact2 += precio;
-		}
-		System.out.println("Factura 2" );
-		System.out.println("Cliente: " + factura2.cliente());
-		System.out.println("Total: " + totalValorFact2);
-	}
+    // Método para calcular el total de una factura sumando los precios de los productos.
+    static double calcularTotal(factura factura) {
+        double total = 0;
+        for (Double precio : factura.preciosProductos) {
+            total += precio;
+        }
+        return total;
+    }
 
+    // Método para imprimir la información de una factura.
+    static void imprimirFactura(factura factura, double total) {
+        System.out.println("Factura");
+        System.out.println("Cliente: " + factura.cliente());
+        System.out.println("Fecha de Factura: " + factura.fechaFactura());
+        System.out.println("Total: " + total);
+        System.out.println();
+    }
 }
